45db7d5dc1d78e7018484cc67b5ddd8758ca9b35
Aleksandr-Chernyaev <whiteanapa140190@gmail.com>
2024-10-26 18:04:10 +0300

    Arrays_1_HW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,91 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // task 1
+        System.out.println("Задача 1");
+
+        int[] firstArrays = new int[]{1, 2, 3};
+        double[] secondArrays = new double[]{1.57, 7.654, 9.986};
+        char[] thirdArrays = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        // task 2
+        System.out.println("Задача 2");
+
+        for (int i = 0; i < firstArrays.length; i++) {
+            System.out.print(firstArrays[i]);
+
+            if (i != firstArrays.length - 1) {
+                System.out.print(", ");
+            }
         }
+
+        System.out.println();
+
+        for (int i = 0; i < secondArrays.length; i++) {
+            System.out.print(secondArrays[i]);
+
+            if (i != secondArrays.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < thirdArrays.length; i++) {
+            System.out.print(thirdArrays[i]);
+
+            if (i != thirdArrays.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+        // task 3
+        System.out.println("Задача 3");
+
+        for (int i = firstArrays.length - 1; i >= 0; i--) {
+            System.out.print(firstArrays[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+        for (int i = secondArrays.length - 1; i >= 0; i--) {
+            System.out.print(secondArrays[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+        for (int i = thirdArrays.length - 1; i >= 0; i--) {
+            System.out.print(thirdArrays[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println();
+
+        //  task 4
+        System.out.println("Задача 4");
+
+        for (int i = 0; i < firstArrays.length; i++) {
+            if (firstArrays[i] % 2 != 0) {
+                firstArrays[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(firstArrays));
+
     }
 }
